fb572cb7b58f1ad1b07bb2951b8f5326b727ce47
federico <federico@gmail.com.ar>
2017-11-27 15:21:14 -0300

    Primer commit - test: Verde

---
+++ CalculadoraTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Calculadora as CA;
+
+final class CalculadoraTest extends TestCase
+{
+	
+	public function testSuma()
+	{
+		$calc= new CA();
+		$this->assertEquals(7,$calc->suma(3,4));
+	}
+
+	public function testFailure()
+    {
+    	$array = new CA();
+        $this->assertArrayHasKey('foo', $array->cargarArray(['foo' => 'baz']));
+    }
+}
+
+?>
